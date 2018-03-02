2bafd3f349c149dd6122b0bc4c058178eb537bc1
abhinavtk97 <abhinavtk97@gmail.com>
2018-03-03 02:30:25 +0530

    voyager

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,8 @@ Route::prefix('admin')->group(function (){
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
     Route::get('/', 'AdminController@index')->name('admin.dashboard');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
